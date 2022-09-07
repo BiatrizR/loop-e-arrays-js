5ee3690574d27a9cafffeaade0e11a43dfa3005f
Bia <82560900+BiatrizR@users.noreply.github.com>
2022-09-07 19:18:40 -0300

    Create arrays_loops.js

---
+++ arrays_loops.js
@@ -0,0 +1,59 @@
+//criando um array = que é uma possibilidade 
+//de armazenar uma lista dentro de uma variavel
+
+//ingredientes do bolo
+var array = [];
+console.log(array);
+
+//adicionado itens no array/lista
+array = ["ovo", "farinha", "leite", "açucar"]
+console.log(array);
+
+//cumprimento do array
+console.log(array.length);
+
+//pegando o último indice do array
+console.log(array.length - 1);
+
+//pegando um unico item da lista
+console.log(array[2]);
+ //quero comprar mais item 
+ console.log(array.push("cacau em pó"));
+ console.log(array);
+
+ //tirar um item da lista
+ console.log(array.pop());
+ console.log(array);
+
+// desafio 
+ 
+ //1. criar uma lista de cargos 3>= estagiario, analista, chefe
+ //2. exibir o analista 
+ //3. contratar alguem => estagiário2; 
+ 
+ var funcionarios = ["estagiario", "", ""]
+
+ //Loops de repetição
+
+ //inicio- fim - incremento(de quanto em quanto)
+ //incrementando de 1 em 1 até 10;
+for(var texto = 0; texto <=10; texto++ ){
+        console.log(texto);
+}
+
+for(var texto = 0; texto <=10; texto++ ){
+    console.log("Eu amo programar!!<3");
+}
+
+ //incrementando de 2 em 2 até 20;
+ for(var i = 2; i <=20; i += 2){
+    console.log(i);
+}
+ 
+
+
+
+
+ 
+
+
